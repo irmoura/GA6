1f663749a87e1268270aaf4ecb2307ca37a19451
Ismael Ribeiro <Ismael Ribeiro@DESKTOP-NHM07EE>
2016-09-21 23:45:51 -0300

    * Correção para 010010.

--- GA6/src/CODIGOS/Tela.java
+++ GA6/src/CODIGOS/Tela.java
@@ -75,6 +75,11 @@ public class Tela extends javax.swing.JFrame {
                     v = v - 3;
                 }
                 //////////////////////////////////////////////////////////////////////
+                /*SE APENAS O 2º E O 5º TÉCNICO ESTIVEREM HABILITADOS*/
+                if(v == 6 && BTN1.isSelected() && !BTN2.isSelected() && BTN3.isSelected() && BTN4.isSelected() && !BTN5.isSelected() && BTN6.isSelected()){
+                    v = v - 4;
+                }
+                //////////////////////////////////////////////////////////////////////
                 /*SE APENAS O 2º E O 3º TÉCNICO ESTIVEREM HABILITADOS*/
                 if(v == 4 && BTN1.isSelected() && !BTN2.isSelected() && !BTN3.isSelected() && BTN4.isSelected() && BTN5.isSelected() && BTN6.isSelected()){
                     v = v - 2;
