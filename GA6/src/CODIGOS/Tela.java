ed60ffa4d0394425067c3f247202c0fd25aab07d
irmoura <irmoura@F02DTIBSW1001.ibyte.corp>
2016-09-22 14:02:42 -0300

    * Correções para controle de horário.

--- GA6/src/CODIGOS/Tela.java
+++ GA6/src/CODIGOS/Tela.java
@@ -7,7 +7,9 @@ package CODIGOS;
 
 import static CODIGOS.Planilha.PS;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.util.Calendar;
+import java.util.Random;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.Timer;
@@ -21,7 +23,7 @@ public class Tela extends javax.swing.JFrame {
     public Color COR_BTN_HABILITADO = Color.BLACK;
     public Color COR_BTN_DESABILITADO = Color.RED;
     
-    public int v;
+    public int v, contador, contador_piada, contador_chamar_novamente,t0,t1,t2,t3,t4,t5,t6;
     public int TDA;
     public int AT1, AT2, AT3, AT4, AT5, AT6;
     
@@ -255,7 +257,8 @@ public class Tela extends javax.swing.JFrame {
         TEXTO_TOTAL = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(988, 382));
+        setTitle("Gerenciador de Atendimento Alpha Version - Desenvolvedor Ismael Ribeiro Moura");
+        setPreferredSize(new java.awt.Dimension(968, 382));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
@@ -267,67 +270,97 @@ public class Tela extends javax.swing.JFrame {
         BTN1.setText("Técnico 1 - 10");
         BTN1.setBorder(null);
         BTN1.setContentAreaFilled(false);
+        BTN1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN1MouseEntered(evt);
+            }
+        });
         BTN1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN1ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 319, -1, -1));
+        getContentPane().add(BTN1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 290, -1, -1));
 
         BTN2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN2.setText("Técnico 2 - 10");
         BTN2.setBorder(null);
         BTN2.setContentAreaFilled(false);
+        BTN2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN2MouseEntered(evt);
+            }
+        });
         BTN2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN2ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN2, new org.netbeans.lib.awtextra.AbsoluteConstraints(184, 319, -1, -1));
+        getContentPane().add(BTN2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 290, -1, -1));
 
         BTN3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN3.setText("Técnico 3 - 10");
         BTN3.setBorder(null);
         BTN3.setContentAreaFilled(false);
+        BTN3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN3MouseEntered(evt);
+            }
+        });
         BTN3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN3ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN3, new org.netbeans.lib.awtextra.AbsoluteConstraints(358, 319, -1, -1));
+        getContentPane().add(BTN3, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 290, -1, -1));
 
         BTN4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN4.setText("Técnico 4 - 10");
         BTN4.setBorder(null);
         BTN4.setContentAreaFilled(false);
+        BTN4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN4MouseEntered(evt);
+            }
+        });
         BTN4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN4ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN4, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 319, -1, -1));
+        getContentPane().add(BTN4, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 290, -1, -1));
 
         BTN5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN5.setText("Técnico 5 - 10");
         BTN5.setBorder(null);
         BTN5.setContentAreaFilled(false);
+        BTN5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN5MouseEntered(evt);
+            }
+        });
         BTN5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN5ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN5, new org.netbeans.lib.awtextra.AbsoluteConstraints(674, 319, -1, -1));
+        getContentPane().add(BTN5, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 290, -1, -1));
 
         BTN6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN6.setText("Técnico 6 - 10");
         BTN6.setBorder(null);
         BTN6.setContentAreaFilled(false);
+        BTN6.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BTN6MouseEntered(evt);
+            }
+        });
         BTN6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 BTN6ActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN6, new org.netbeans.lib.awtextra.AbsoluteConstraints(836, 319, -1, -1));
+        getContentPane().add(BTN6, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 290, -1, -1));
 
         BTN_CHAMAR.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         BTN_CHAMAR.setText("CHAMAR");
@@ -338,7 +371,7 @@ public class Tela extends javax.swing.JFrame {
                 BTN_CHAMARActionPerformed(evt);
             }
         });
-        getContentPane().add(BTN_CHAMAR, new org.netbeans.lib.awtextra.AbsoluteConstraints(347, 11, -1, -1));
+        getContentPane().add(BTN_CHAMAR, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 10, -1, -1));
 
         TEXTO_NOME_DA_VEZ.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         TEXTO_NOME_DA_VEZ.setText("Técnico 1 - 1111");
@@ -353,7 +386,7 @@ public class Tela extends javax.swing.JFrame {
                 BOTAO_ZERARActionPerformed(evt);
             }
         });
-        getContentPane().add(BOTAO_ZERAR, new org.netbeans.lib.awtextra.AbsoluteConstraints(546, 11, -1, -1));
+        getContentPane().add(BOTAO_ZERAR, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 10, -1, -1));
 
         TEXTO_TOTAL.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         TEXTO_TOTAL.setText("Atendimentos :");
@@ -553,6 +586,137 @@ public class Tela extends javax.swing.JFrame {
                 BTN5.setText(A6);
                 BTN6.setText(A7);
         ////////////////////////////////////////////////////////////////////////
+        /*   ENTRADA   */
+        if(Integer.parseInt(C2) < 10 && Integer.parseInt(D2) < 10){
+        ET1 = "0"+C2+":0"+D2+":00";
+          }else
+        if(Integer.parseInt(C2) > 9 && Integer.parseInt(D2) > 9){
+        ET1 = C2+":"+D2+":00";
+        }else
+        if(Integer.parseInt(C2) < 10 && Integer.parseInt(D2) > 9){
+        ET1 = "0"+C2+":"+D2+":00";
+        }else
+        if(Integer.parseInt(C2) > 9 && Integer.parseInt(D2) < 10){
+        ET1 = C2+":0"+D2+":00";
+        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[14]) < 10 && Integer.parseInt(PS[15]) < 10){
+//        PSL3[6] = "0"+PS[14]+":0"+PS[15]+":00";
+//        }else
+//        if(Integer.parseInt(PS[14]) > 9 && Integer.parseInt(PS[15]) > 9){
+//        PSL3[6] = PS[14]+":"+PS[15]+":00";
+//        }else
+//        if(Integer.parseInt(PS[14]) < 10 && Integer.parseInt(PS[15]) > 9){
+//        PSL3[6] = "0"+PS[14]+":"+PS[15]+":00";
+//        }else
+//        if(Integer.parseInt(PS[14]) > 9 && Integer.parseInt(PS[15]) < 10){
+//        PSL3[6] = PS[14]+":0"+PS[15]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[20]) < 10 && Integer.parseInt(PS[21]) < 10){
+//        PSL4[6] = "0"+PS[20]+":0"+PS[21]+":00";
+//        }else
+//        if(Integer.parseInt(PS[20]) > 9 && Integer.parseInt(PS[21]) > 9){
+//        PSL4[6] = PS[20]+":"+PS[21]+":00";
+//        }else
+//        if(Integer.parseInt(PS[20]) < 10 && Integer.parseInt(PS[21]) > 9){
+//        PSL4[6] = "0"+PS[20]+":"+PS[21]+":00";
+//        }else
+//        if(Integer.parseInt(PS[20]) > 9 && Integer.parseInt(PS[21]) < 10){
+//        PSL4[6] = PS[20]+":0"+PS[21]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[26]) < 10 && Integer.parseInt(PS[27]) < 10){
+//        PSL5[6] = "0"+PS[26]+":0"+PS[27]+":00";
+//        }else
+//        if(Integer.parseInt(PS[26]) > 9 && Integer.parseInt(PS[27]) > 9){
+//        PSL5[6] = PS[26]+":"+PS[27]+":00";
+//        }else
+//        if(Integer.parseInt(PS[26]) < 10 && Integer.parseInt(PS[27]) > 9){
+//        PSL5[6] = "0"+PS[26]+":"+PS[27]+":00";
+//        }else
+//        if(Integer.parseInt(PS[26]) > 9 && Integer.parseInt(PS[27]) < 10){
+//        PSL5[6] = PS[26]+":0"+PS[27]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[32]) < 10 && Integer.parseInt(PS[33]) < 10){
+//        PSL6[6] = "0"+PS[32]+":0"+PS[33]+":00";
+//        }else
+//        if(Integer.parseInt(PS[32]) > 9 && Integer.parseInt(PS[33]) > 9){
+//        PSL6[6] = PS[32]+":"+PS[33]+":00";
+//        }else
+//        if(Integer.parseInt(PS[32]) < 10 && Integer.parseInt(PS[33]) > 9){
+//        PSL6[6] = "0"+PS[32]+":"+PS[33]+":00";
+//        }else
+//        if(Integer.parseInt(PS[32]) > 9 && Integer.parseInt(PS[33]) < 10){
+//        PSL6[6] = PS[32]+":0"+PS[33]+":00";
+//        }
+//        
+//        /*   SAIDA   */
+//        if(Integer.parseInt(PS[10]) < 10 && Integer.parseInt(PS[11]) < 10){
+//        PSL2[7] = "0"+PS[10]+":0"+PS[11]+":00";
+//        }else
+//        if(Integer.parseInt(PS[10]) > 9 && Integer.parseInt(PS[11]) > 9){
+//        PSL2[7] = PS[10]+":"+PS[11]+":00";
+//        }else
+//        if(Integer.parseInt(PS[10]) > 9 && Integer.parseInt(PS[11]) < 10){
+//        PSL2[7] = PS[10]+":0"+PS[11]+":00";
+//        }else
+//        if(Integer.parseInt(PS[10]) < 10 && Integer.parseInt(PS[11]) > 9){
+//        PSL2[7] = "0"+PS[10]+":"+PS[11]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[16]) < 10 && Integer.parseInt(PS[17]) < 10){
+//        PSL3[7] = "0"+PS[16]+":0"+PS[17]+":00";
+//        }else
+//        if(Integer.parseInt(PS[16]) > 9 && Integer.parseInt(PS[17]) > 9){
+//        PSL3[7] = PS[16]+":"+PS[17]+":00";
+//        }else
+//        if(Integer.parseInt(PS[16]) > 9 && Integer.parseInt(PS[17]) < 10){
+//        PSL3[7] = PS[16]+":0"+PS[17]+":00";
+//        }else
+//        if(Integer.parseInt(PS[16]) < 10 && Integer.parseInt(PS[17]) > 9){
+//        PSL3[7] = "0"+PS[16]+":"+PS[17]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[22]) < 10 && Integer.parseInt(PS[23]) < 10){
+//        PSL4[7] = "0"+PS[22]+":0"+PS[23]+":00";
+//        }else
+//        if(Integer.parseInt(PS[22]) > 9 && Integer.parseInt(PS[23]) > 9){
+//        PSL4[7] = PS[22]+":"+PS[23]+":00";
+//        }else
+//        if(Integer.parseInt(PS[22]) > 9 && Integer.parseInt(PS[23]) < 10){
+//        PSL4[7] = PS[22]+":0"+PS[23]+":00";
+//        }else
+//        if(Integer.parseInt(PS[22]) < 10 && Integer.parseInt(PS[23]) > 9){
+//        PSL4[7] = "0"+PS[22]+":"+PS[23]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[28]) < 10 && Integer.parseInt(PS[29]) < 10){
+//        PSL5[7] = "0"+PS[28]+":0"+PS[29]+":00";
+//        }else
+//        if(Integer.parseInt(PS[28]) > 9 && Integer.parseInt(PS[29]) > 9){
+//        PSL5[7] = PS[28]+":"+PS[29]+":00";
+//        }else
+//        if(Integer.parseInt(PS[28]) > 9 && Integer.parseInt(PS[29]) < 10){
+//        PSL5[7] = PS[28]+":0"+PS[29]+":00";
+//        }else
+//        if(Integer.parseInt(PS[28]) < 10 && Integer.parseInt(PS[29]) > 9){
+//        PSL5[7] = "0"+PS[28]+":"+PS[29]+":00";
+//        }
+//        ////////////////////////////////////////////////////////////////////////
+//        if(Integer.parseInt(PS[34]) < 10 && Integer.parseInt(PS[35]) < 10){
+//        PSL6[7] = "0"+PS[34]+":0"+PS[35]+":00";
+//        }else
+//        if(Integer.parseInt(PS[34]) > 9 && Integer.parseInt(PS[35]) > 9){
+//        PSL6[7] = PS[34]+":"+PS[35]+":00";
+//        }else
+//        if(Integer.parseInt(PS[34]) > 9 && Integer.parseInt(PS[35]) < 10){
+//        PSL6[7] = PS[34]+":0"+PS[35]+":00";
+//        }else
+//        if(Integer.parseInt(PS[34]) < 10 && Integer.parseInt(PS[35]) > 9){
+//        PSL6[7] = "0"+PS[34]+":"+PS[35]+":00";
+//        }
         ////////////////////////////////////////////////////////////////////////
         HCT1 = Integer.parseInt(C2);//Hora Chegada Técnico 1
         MCT1 = Integer.parseInt(D2);//Minuto chegada Técnico 1
@@ -595,8 +759,148 @@ public class Tela extends javax.swing.JFrame {
         MST6 = Integer.parseInt(F7);//Minuto Saida Técnico 5
 //        ET6 = PSL6[6];//Entrada Técnico 5
 //        ST6 = PSL6[7];//Saida Técnico 5
+
+        ////////////////////////////////////////////////////////////////////////
+         timer = new Timer(1000, (ActionEvent e) -> {
+            
+            contador++;
+            
+            /////////////////////////SEGUNDOS/////////////////////////
+                if(contador < 10){
+                    segundos = "0"+contador;
+                }
+                if(contador > 9){
+                    segundos = ""+contador;
+                }
+                if(contador == 60){
+                    contador = 0;
+                    segundos = "0"+contador;
+                    minutos_int++;
+                }
+                /////////////////////////SEGUNDOS/////////////////////////
+                /////////////////////////MINUTOS/////////////////////////
+                if(minutos_int == 0){
+                    minutos = "00";
+                }
+                if(minutos_int < 10){
+                    minutos = "0"+minutos_int;
+                }
+                if(minutos_int > 9){
+                    minutos = ""+minutos_int;
+                }
+                if(minutos_int == 60){
+                    minutos_int = 0;
+                    minutos = "0"+minutos_int;
+                    horas_int++;
+                }
+                /////////////////////////MINUTOS/////////////////////////
+                /////////////////////////HORAS/////////////////////////
+                if(horas_int == 0){
+                    HC = "00";
+                }
+                if(horas_int < 10){
+                    HC = "0"+horas_int;
+                }
+                if(horas_int > 9){
+                    HC = ""+horas_int;
+                }
+                if(horas_int == 24){
+                    horas_int = 0;
+                    HC = "0"+horas_int;
+                }
+              
+              obterHoras();
+
+              ////////////////////////////////////////////////////////////////////
+            /*TÉCNICOS HABILITADOS NA HORA E MINUTO DEFINIDOS*/
+            if(horas.equals(ET1)){
+                BTN1.setEnabled(true);
+                BTN1.setSelected(false);
+                BTN1.setForeground(Color.black);
+            }
+            if(horas.equals(ET2)){
+                BTN2.setEnabled(true);
+                BTN2.setSelected(false);
+                BTN2.setForeground(Color.black);
+            }
+            if(horas.equals(ET3)){
+                BTN3.setEnabled(true);
+                BTN3.setSelected(false);
+                BTN3.setForeground(Color.black);
+            }
+            if(horas.equals(ET4)){
+                BTN4.setEnabled(true);
+                BTN4.setSelected(false);
+                BTN4.setForeground(Color.black);
+            }
+            if(horas.equals(ET5)){
+                BTN5.setEnabled(true);
+                BTN5.setSelected(false);
+                BTN5.setForeground(Color.black);
+            }
+                ////////////////////////////////////////////////////////////////////
+            /*TÉCNICOS DESABILITADOS NA HORA E MINUTO DEFINIDOS*/
+            if(horas.equals(ST1)){
+                BTN1.setEnabled(false);
+                BTN1.setSelected(true);
+                BTN1.setForeground(Color.red);
+            }
+            if(horas.equals(ST2)){
+                BTN2.setEnabled(false);
+                BTN2.setSelected(true);
+                BTN2.setForeground(Color.red);
+            }
+            if(horas.equals(ST3)){
+                BTN3.setEnabled(false);
+                BTN3.setSelected(true);
+                BTN3.setForeground(Color.red);
+            }
+            if(horas.equals(ST4)){
+                BTN4.setEnabled(false);
+                BTN4.setSelected(true);
+                BTN4.setForeground(Color.red);
+            }
+            if(horas.equals(ST5)){
+                BTN5.setEnabled(false);
+                BTN5.setSelected(true);
+                BTN5.setForeground(Color.red);
+            }
+           
+        });
+        
+        timer.start();
         
     }//GEN-LAST:event_formWindowOpened
+
+    private void BTN1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN1MouseEntered
+        // TODO add your handling code here:
+        BTN1.setToolTipText(ET1+" às "+ST1);
+    }//GEN-LAST:event_BTN1MouseEntered
+
+    private void BTN2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN2MouseEntered
+        // TODO add your handling code here:
+        BTN2.setToolTipText(ET2+" às "+ST2);
+    }//GEN-LAST:event_BTN2MouseEntered
+
+    private void BTN3MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN3MouseEntered
+        // TODO add your handling code here:
+        BTN3.setToolTipText(ET3+" às "+ST3);
+    }//GEN-LAST:event_BTN3MouseEntered
+
+    private void BTN4MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN4MouseEntered
+        // TODO add your handling code here:
+        BTN4.setToolTipText(ET4+" às "+ST4);
+    }//GEN-LAST:event_BTN4MouseEntered
+
+    private void BTN5MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN5MouseEntered
+        // TODO add your handling code here:
+        BTN5.setToolTipText(ET5+" às "+ST5);
+    }//GEN-LAST:event_BTN5MouseEntered
+
+    private void BTN6MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BTN6MouseEntered
+        // TODO add your handling code here:
+        BTN6.setToolTipText(ET6+" às "+ST6);
+    }//GEN-LAST:event_BTN6MouseEntered
 
     /**
      * @param args the command line arguments
